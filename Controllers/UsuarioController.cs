1b0d0b91b508e4e48ba5106419ca8412bf04cbc9
greensystem2000 <greensystem2000@gmail.com>
2020-11-03 21:59:47 -0300

    POSTExample

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using project1.Models;
+
+namespace project1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpPost]
+        public string Post([FromBody] UsuarioModel usuario) {
+            return usuario.Name == "Davi" && usuario.Password == "123" ? "OK" : "ERRO";
+        }
+    }
+}
